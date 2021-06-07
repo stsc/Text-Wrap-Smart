c0a2ef0d4fe6ed3eeaf6afe90f137f5501211e14
Steven Schubiger <schubiger@cpan.org>
2021-06-07 17:48:22 +0200

    released 0.9

--- lib/Text/Wrap/Smart.pm
+++ lib/Text/Wrap/Smart.pm
@@ -12,7 +12,7 @@ use Params::Validate ':all';
 our ($VERSION, @EXPORT_OK, %EXPORT_TAGS);
 my @subs;
 
-$VERSION = '0.8_01';
+$VERSION = '0.9';
 @subs = qw(exact_wrap fuzzy_wrap);
 @EXPORT_OK = @subs;
 %EXPORT_TAGS = ('all' => [ @subs ]);
